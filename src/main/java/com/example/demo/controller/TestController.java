64bc7266d6219ed30c0423119723782941777c7d
moujie <moujie@manlinggame.com>
2021-07-07 18:59:06 +0800

    test

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author moujie
+ * @className TestController
+ * @description TODO 类描述
+ * @date 2021/7/7
+ **/
+@RestController
+@RequestMapping("/demo")
+public class TestController {
+
+	@RequestMapping("/test/{name}")
+	public String test(@PathVariable("name") String name) {
+       	return "welcome here "+ name +" !";
+	}
+}
